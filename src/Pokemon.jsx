bd78a3c615a583e2de7a25883dbee9f9a4e480ef
Kailash Badu <152411431+404bad@users.noreply.github.com>
2026-04-06 22:24:49 +0545

    why not

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -65,7 +65,7 @@ export const Pokemon = () => {
     <>
       <section className="container">
         <header>
-          <h1> Lets Catch Pokémon for Kailash Badu.This is fifth try.</h1>
+          <h1> Lets Catch Pokémon for Kailash Badu.This is sixth try.</h1>
         </header>
         <div className="pokemon-search">
           <input
