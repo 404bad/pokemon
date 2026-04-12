ebf233901553880d9a7547e11f77ab9acd44a2a3
Kailash Badu <152411431+404bad@users.noreply.github.com>
2026-04-12 21:01:08 +0545

    change

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -68,7 +68,6 @@ export const Pokemon = () => {
           <h1> Kailash's Pokemon Arsenal.</h1>
         </header>
         <div className="pokemon-search">
-          <input placeholder="This is after code pipeline." /><br/>
           <input
             type="text"
             placeholder="search Pokemon"
