c78a24f3fc3e708f3611d969ba9a665cbd7c5fa9
Kailash Badu <152411431+404bad@users.noreply.github.com>
2026-04-13 16:38:00 +0545

    change aws  login

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 533267438223.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 290657649733.dkr.ecr.us-east-1.amazonaws.com
 
 cd /home/ubuntu/pokemon-app
 
